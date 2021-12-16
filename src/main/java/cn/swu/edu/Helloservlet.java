19644b8d8667fb9e62950aa3b57aa644d2911b0d
emmmmYeah <1776283041@qq.com>
2021-12-16 15:51:59 +0800

    Initial commit

---
+++ src/main/java/cn/swu/edu/Helloservlet.java
@@ -0,0 +1,30 @@
+package cn.swu.edu;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Helloservlet extends HttpServlet {
+    private String message;
+
+    private static final long serialVersionUID = 1198763442711986380L;
+
+    public void init() {
+        message = "Hello, my dear!";
+
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        this.doPost(request, response);
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out= response.getWriter();
+        out.println("<h1>"+message+"</h1>");
+
+    }
+}
